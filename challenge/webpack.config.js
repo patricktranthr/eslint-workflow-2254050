f94bcffe6f94d269432482b9e9fd4576c31241b8
patricktranthr <patrick.trant.hr@gmail.com>
2024-06-13 11:25:31 +0000

    Coures outdated, can't do challenge

--- challenge/webpack.config.js
+++ challenge/webpack.config.js
@@ -1,20 +1,26 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: "./src/index.js",
   output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
   },
   module: {
     rules: [
       {
+        enforce: "pre",
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "eslint-loader",
+      },
+      {
         test: /\.js$/,
         exclude: /(node_modules)/,
         use: {
-          loader: 'babel-loader',
+          loader: "babel-loader",
           options: {
-            presets: ['@babel/preset-env'],
+            presets: ["@babel/preset-env"],
           },
         },
       },
